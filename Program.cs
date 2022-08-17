a5d9a40d5cc79d78bc8aaa3656b08577c6510b9b
ksergeyru <i@ksergey.ru>
2022-08-17 15:52:03 +0300

    commit 500

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+// app.MapGet("/", () => "Hello World!");
+// app.Run();
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+
+
+app.UseRouting();
+
+app.UseEndpoints(e =>
+{
+  // http://localhost:8812/home/index
+  e.MapControllerRoute("default", "{controller}/{action}");
+});
 app.Run();
+
 
 // MVC
 
